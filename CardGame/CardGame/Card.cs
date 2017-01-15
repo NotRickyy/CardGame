e77e57b3582ba373be40ede7c2b59a71753fc0d7
michaeltran0 <hautran@live.ca>
2017-01-15 10:45:46 -0500

    Added Card Methods
    added methods compare and getSuitString.
    changed suit to int.

--- CardGame/CardGame/Card.cs
+++ CardGame/CardGame/Card.cs
@@ -8,10 +8,10 @@ namespace CardGame
 {
     class Card
     {
-        private String suit;
+        private int suit;//4=hearts, 3=diamonds, 2=clubs, 1=spades.  Order can be changed if needed.
         private int value;
 
-        public string Suit
+        public int Suit
         {
             get
             {
@@ -34,6 +34,54 @@ namespace CardGame
             set
             {
                 this.value = value;
+            }
+        }
+
+        public String getSuitString()
+        {
+            switch (this.suit)
+            {
+                case 1:
+                    return "Hearts";
+                    break;
+                case 2:
+                    return "Diamonds";
+                    break;
+                case 3:
+                    return "Clubs";
+                    break;
+                case 4:
+                    return "Spades";
+                    break;
+                default:
+                    return "Error";
+                    break;
+            }
+        }
+
+        //compare card, set chkSuit to true to compare the suits as well
+        public String compare(Card card, Boolean chkSuit)
+        {
+            if (card.value > this.value)
+            {
+                return "Higher";
+            }else if (card.value < this.value)
+            {
+                return "Lower";
+            }else if (card.value == this.value)
+            {
+                return "Equal";
+                if (chkSuit == true)
+                {
+                    if (card.suit > this.suit)
+                    {
+                        return "Higher";
+                    }
+                    else if (card.suit < this.suit)
+                    {
+                        return "Lower";
+                    }
+                }
             }
         }
     }
