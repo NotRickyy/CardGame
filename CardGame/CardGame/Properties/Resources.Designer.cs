0ede95964659a92023e078496355da6d6ee9c075
michaeltran0 <hautran@live.ca>
2017-01-22 23:44:17 -0500

    Fixed "Back1" error

--- CardGame/CardGame/Properties/Resources.Designer.cs
+++ CardGame/CardGame/Properties/Resources.Designer.cs
@@ -69,7 +69,19 @@ namespace CardGame.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Back1
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("Back1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
